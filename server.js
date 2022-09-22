5d92946e70114d454c4c3760bc356b3cd410ce83
kevinkorza <kevingkorza@gmail.com>
2022-09-22 18:11:39 -0400

    connects to data base

--- server.js
+++ server.js
@@ -19,3 +19,17 @@ const db = mysql.createConnection(
     console.log('Connected to the databse.')
 );
 
+
+//
+db.query('SELECT * FROM department', function (err, results) {
+    console.log(results);
+});
+
+//Default response for any other request (NOT FOUND)
+app.use((req, res) => {
+    res.status(404).end();
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+});
