abcaa1f0298b8263ae1b0e1f7e7772ff5a0412c3
kevinkorza <kevingkorza@gmail.com>
2022-09-27 15:44:18 -0400

    schema, seeds, and query are complete

--- db/query.sql
+++ db/query.sql
@@ -1,12 +1,3 @@
---SELECT *
---FROM course_names
---JOIN department ON course_names.department = department.id;
-
-
---SELECT * FROM department;
-
-
---SELECT * FROM role;
-
-
---SELECT * FROM employee;
+SELECT *
+FROM columns to select from
+JOIN table to join ON FOREIGN KEY = PRIMARY KEY on joined table;
